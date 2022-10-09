bacc6b33de174ee56008a3351bb6790a4d657fdb
Arnau Oriol <arnauoriol1996@hotmail.com>
2022-10-09 10:35:56 +0200

    Remove Cell class and everything related to a false

--- public/js/buildBoard/buildBoard.test.js
+++ public/js/buildBoard/buildBoard.test.js
@@ -1,14 +1,13 @@
 import buildBoard from "./buildBoard.js";
-import Cell from "../Cell/Cell.js";
 
 describe("Given the buildBoard function", () => {
   describe("When it receives 3", () => {
-    test("Then it should return [[Cell, Cell, Cell],[Cell, Cell, Cell],[Cell, Cell, Cell]]", () => {
+    test("Then it should return [[false, false, false],[false, false, false],[false, false, false]]", () => {
       const boardSize = 3;
       const expectedBoard = [
-        [new Cell(0, 0), new Cell(0, 1), new Cell(0, 2)],
-        [new Cell(1, 0), new Cell(1, 1), new Cell(1, 2)],
-        [new Cell(2, 0), new Cell(2, 1), new Cell(2, 2)],
+        [false, false, false],
+        [false, false, false],
+        [false, false, false],
       ];
 
       const newBoard = buildBoard(boardSize);
